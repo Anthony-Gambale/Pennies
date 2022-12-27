9e3ed11781effdaeac4bfb49cf44d5eb1725fa89
Anthony Gambale <ants.gambale@gmail.com>
2022-12-27 21:45:30 +1100

    Tested prev() and next() on 3 million cases

--- src/EquivalenceClassTest.java
+++ src/EquivalenceClassTest.java
@@ -4,14 +4,19 @@ import static org.junit.jupiter.api.Assertions.*;
 class EquivalenceClassTest {
 
     @Test
-    public void testPrevSimple() {
+    public void testPrev() {
         // prev().next() should be the identity operation
-
+        // 3 million cases, should take no longer than a minute
+        for (int i = 1; i < 150; i++) {
+            for (int j = 1; j < 150; j++) {
+                for (int k = 1; k < 150; k++) {
+                    EquivalenceClass x = new EquivalenceClass(new int[] {i,j,k,i,i,j,i,j});
+                    for (EquivalenceClass y : x.prev()) {
+                        EquivalenceClass z = y.next();
+                        assertEquals(z.piles, x.piles, "Broken case: program says\n" + y + "\nis a predecessor class of\n" + x + "\nbut its next() method returns\n" + z);
+                    }
+                }
+            }
+        }
     }
-
-    @Test
-    public void testPrevComplex() {
-        // prev().prev().next().next() should be the identity operation
-    }
-
 }
